c44a36e1f59f85dd4803bdb78b5cc8472783d7f8
c20245091yabut <c20245091.yabut@csav.edu.ph>
2026-09-11 09:53:38 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ console.log ("hello world");
 console.log ("first java script");  
 
 
+const myName = "Nicolas";
+let age = 20;
+const number = 940312345713;
+const address = "Brgy 1-A manapla ";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
